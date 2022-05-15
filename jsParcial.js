546ba6c2fa1970f4d8168490b3c27946b3727dce
Fernando <frodriguezok@gmail.com>
2022-05-15 17:00:39 -0300

    Requerimientos 1,2,3 realizados

--- jsParcial.js
+++ jsParcial.js
@@ -1,5 +1,3 @@
-
-
 //Funciones de requerimiento 1
 
 function sumarVistas() {
@@ -63,27 +61,79 @@ function buscarPalabra() {
 
 // Funciones de requerimiento 2
 
+function validarIngresos(unaSerie,cantidad) {
+    if (unaSerie == "" || cantidad == "") {
+        return true
+    }
+}
+
 function guardarSerie(){
     let tabla = document.getElementById('idTabla').insertRow(-1)
     let nombreSerie = document.getElementById("unNombre").value
     let cantidadTemporadas = document.getElementById("cantidadTemporadas").value
-    console.log(nombreSerie.value)
-    console.log(cantidadTemporadas.value)
-    let col1 = tabla.insertCell(0)
-    let col2 = tabla.insertCell(1)
-    let col3 = tabla.insertCell(2)
-    let col4 = tabla.insertCell(3)
-    let col5 = tabla.insertCell(4)
+    console.log(nombreSerie)
+    console.log(cantidadTemporadas)
+    if(validarIngresos(nombreSerie,cantidadTemporadas)) {
+        alert("Complete todos los campos para poder editar por favor")
+    } else {
+        let col1 = tabla.insertCell(0)
+        let col2 = tabla.insertCell(1)
+        let col3 = tabla.insertCell(2)
+        let col4 = tabla.insertCell(3)
+        let col5 = tabla.insertCell(4)
+    
+        col1.innerHTML = '<input type="checkbox" name="tilde" value="selecciona" >';
+        col2.innerHTML = nombreSerie;
+        col3.innerHTML = cantidadTemporadas
+        col4.innerHTML = 0; 
+        col5.innerHTML = 0 +"%";
+    
+        col2.classList.add("nombreDeSerie");
+        col3.classList.add("temporada");
+        col4.classList.add("vistas");
+        col5.classList.add("porcentaje");
+    }
+}
 
-    col1.innerHTML = '<input type="checkbox" name="tilde" value="selecciona" >';
-    col2.innerHTML = nombreSerie;
-    col3.innerHTML = cantidadTemporadas
-    col4.innerHTML = 0; 
-    col5.innerHTML = 0 +"%";
+function editarSerie() {
+    let vectorCheck = document.querySelectorAll('input[type=checkbox]')
+    for (let i = 0; i < vectorCheck.length; i++) {
+        if (vectorCheck[i].checked) {
+            let nombreEditado = document.getElementById('serieEditar').value
+            let temporadaEditada = document.getElementById('temporadasEditar').value
+            let vistasEditada = document.getElementById('vistasEditar').value
+            let nombreDeSeries = document.getElementsByClassName('nombreDeSerie')
+            let temporadaDeSeries = document.getElementsByClassName('temporada')
+            let vistasDeSeries = document.getElementsByClassName('vistas')
+            if (nombreEditado != "") {
+                nombreDeSeries[i].innerHTML = nombreEditado
+            }
+            if (validarIngresos(temporadaEditada,vistasEditada)){
+                alert("Ingrese las temporadas de la serie y las vistas para poder editar por favor")
+            } else {
+                if (parseInt(temporadaEditada) >= parseInt(vistasEditada)){
+                    temporadaDeSeries[i].innerHTML = parseInt(temporadaEditada)
+                    vistasDeSeries[i].innerHTML = parseInt(vistasEditada)
+                    actualizarPorcentaje()
+                } else {
+                    alert("La cantidad de temporadas vistas debe ser menor o igual a la cantidad que tiene la serie")
+                }
+            }
+        }
+    } 
+}
 
-    col3.classList.add("temporada");
-    col4.classList.add("vistas");
-    col5.classList.add("porcentaje");
+function validador(evento){
+    var numero = (evento.which) ? evento.which : evento.keyCode;
+    resultado = 0
+    if(numero>=48 && numero<=57) {
+      resultado = true;
+    } else
+    {
+        alert("Debe ingresar un numero entero");
+        resultado = false;
+    }
+    return resultado
 }
 
 // Funciones de requerimiento 3
