6d060dda8725f4f7e190ce0e6544b8a864e46535
Fernando <frodriguezok@gmail.com>
2022-05-14 23:57:32 -0300

    Requerimientos 1 y 3 completos + crear

--- jsParcial.js
+++ jsParcial.js
@@ -4,6 +4,7 @@
 
 function sumarVistas() {
     let vectorCheck = document.querySelectorAll('input[type=checkbox]')
+    console.log("El tamaño es: "+vectorCheck.length)
     for (let i = 0; i < vectorCheck.length; i++) {
         const unCheck = vectorCheck[i];
         if (unCheck.checked){
@@ -17,7 +18,6 @@ function sumarVistas() {
                 actualizarPorcentaje()
             }
         }
-
     }
 }
 
@@ -29,7 +29,6 @@ function restarVistas() {
             let vistas = document.getElementsByClassName("vistas")
             let cantidadTemporadas = document.getElementsByClassName("temporada")
             let vistasValor = vistas[i].innerHTML
-            let temporadasValor = cantidadTemporadas[i].innerHTML
             if (vistasValor > 0) {
                 vistasValor = parseInt(vistasValor) - parseInt(1)
                 vistas[i].innerHTML = vistasValor
@@ -38,32 +37,6 @@ function restarVistas() {
         }
     }
 }
-
-
-/*function buscarPalabra() {
-    const unaPalabra = document.getElementById("palabra").value;
-    console.log(unaPalabra)
-    const unaTabla = document.querySelectorAll('tr');
-    console.log(unaTabla)
-    for (let i = 0; i < unaTabla.length; i++) {
-        const unaFila = unaTabla[i];
-        console.log(unaFila)
-        const elementos = document.querySelectorAll('td');
-        for (let j = 0; j < elementos.length; j++) {
-            const element = elementos[j];
-            console.log(element.innerHTML)
-            if (unaPalabra == element.innerHTML) {
-                console.log("Hay una coincidencia")
-                element.style.color = "red";
-            }
-            else {
-                console.log("No se ha encontrado una serie")
-                element.style.color = "white";
-            }
-        }
-    }
-}
-*/
 
 function buscarPalabra() {
     const unaPalabra = document.getElementById("palabra").value;
@@ -91,12 +64,27 @@ function buscarPalabra() {
 // Funciones de requerimiento 2
 
 function guardarSerie(){
-    const unaTabla = document.querySelectorAll('tr')
-    const unNombre = document.getElementById("unNombre").value
-    const numeroTemporadas = document.getElementById('cantidadTemporadas').value
-    unaTabla.insertRow = '<td></td><td>'+unNombre+'</td><td>'+numeroTemporadas+'</td><td>'+"0"+'</td><td>'+"0%"+'</td>'
-  }
+    let tabla = document.getElementById('idTabla').insertRow(-1)
+    let nombreSerie = document.getElementById("unNombre").value
+    let cantidadTemporadas = document.getElementById("cantidadTemporadas").value
+    console.log(nombreSerie.value)
+    console.log(cantidadTemporadas.value)
+    let col1 = tabla.insertCell(0)
+    let col2 = tabla.insertCell(1)
+    let col3 = tabla.insertCell(2)
+    let col4 = tabla.insertCell(3)
+    let col5 = tabla.insertCell(4)
 
+    col1.innerHTML = '<input type="checkbox" name="tilde" value="selecciona" >';
+    col2.innerHTML = nombreSerie;
+    col3.innerHTML = cantidadTemporadas
+    col4.innerHTML = 0; 
+    col5.innerHTML = 0 +"%";
+
+    col3.classList.add("temporada");
+    col4.classList.add("vistas");
+    col5.classList.add("porcentaje");
+}
 
 // Funciones de requerimiento 3
 
@@ -104,7 +92,6 @@ function actualizarPorcentaje() {
     const temporadas = document.getElementsByClassName('temporada');
     const tVistas = document.getElementsByClassName('vistas');
     const porcentaje = document.getElementsByClassName('porcentaje');
-    var calculo = 0
     for (let i = 0; i < temporadas.length; i++) {
         const element = temporadas[i].innerHTML;
         calculo = (tVistas[i].innerHTML/element) * 100;
